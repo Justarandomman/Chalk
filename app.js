99a9ed381b25210a56f9dec19db29faa63a52ef0
BRENTON THOMLINSON <bt0904795@otc.edu>
2019-11-10 13:21:55 -0600

    Storytime

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-var http = require('http');
-const fs = require('fs');
 const chalk = require('chalk');
 
-console.log(chalk.blue('Hello World!'));
-
-var hostname = '127.0.0.1';
-var port = 3000;
-
-fs.readFile('index.html', (err, html) => {
-    if(err){
-        throw err;
-    }
-    var server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/html');
-    res.write(html);
-    res.end();
-});
-
-server.listen(port, hostname, () => {
-    console.log('Server started on port '+port);
-});
-});
-
+console.log(chalk.green('There once was a man from Peru'));
+console.log(chalk.green('Who dreamed he was eating his shoe'));
+console.log(chalk.yellow('He woke with a fright in the middle of the night...'));
+console.log(chalk.red('To find his women and children beside the window, firing rounds at an unseen enemy'));
+console.log(chalk.yellow('With his body shaking from terror, he cried out "What is going on here?!"'));
+console.log(chalk.red('His wife only grimaced towards him, casually tossing a rifle towards the man before her'));
+console.log(chalk.red('"Zombies. And plenty of them"'));
+console.log(chalk.yellow('"Zombies?! I had only slept for a single night, I was supposed to wake up with a shoe in my mouth! how did this happen?!"'));
+console.log(chalk.red('"It was not a single night. When you fell aslumber, it was a stormy night. It was raining, it was pouring, and you were snoring'));
+console.log(chalk.red('"All we know is that when you went to bed and bumped your head, you did not get up in the morning"'));
+console.log(chalk.yellow('The man trembled in place, wondering how this had happened to him. Regardless of his feelings, he lifted the weapon upon his shoulder and grit his teeth'));
+console.log(chalk.yellow('"I wish my dream was real, and I had just eaten my shoe..."'));
